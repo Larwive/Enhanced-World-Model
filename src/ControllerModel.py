4e05c9e8414f74a412eb9bfb23de84fdcd3ac736
Larwive <larwive@gmail.com>
2025-07-03 22:05:34 +0200

    Add base architecture

---
+++ src/ControllerModel.py
@@ -0,0 +1,10 @@
+import torch
+
+
+class ControllerModel:
+    """
+    The base class for controller (C) models.
+    """
+
+    def __init__(self) -> None:
+        pass
---
+++ src/MemoryModel.py
@@ -0,0 +1,10 @@
+import torch
+
+
+class MemoryModel:
+    """
+    The base class for memory (M) models.
+    """
+
+    def __init__(self) -> None:
+        pass
