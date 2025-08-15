7c63471fdbdb14d7854ce350a902ebaad5aba61d
Larwive <larwive@gmail.com>
2025-08-15 20:13:09 +0200

    Add stochastic controller, add REINFORCE loss, finish integration

--- src/main.py
+++ src/main.py
@@ -10,15 +10,18 @@ from vision.VQ_VAE import VQ_VAE
 from vision.Identity import Identity
 from memory.TemporalTransformerXCPC import TemporalTransformer
 from controller.ModelPredictiveController import ModelPredictiveController
+from controller.StochasticController import StochasticController
 from WorldModel import WorldModel
 from train import train
 
 device = torch.device("mps") if torch.backends.mps.is_available() else torch.device(
     "cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env-name', type=str, default='CartPole-v1', help='The Gym environment to use.')
+    parser.add_argument('--env-name', type=str, default='CarRacing-v3',
+                        help='The Gym environment to use.')  # CartPole-v1
     parser.add_argument('--random-seed', type=int, default=42)
     parser.add_argument('--max-epoch', type=int, default=200)
     parser.add_argument('--patience', type=int, default=5)
@@ -36,7 +39,7 @@ def main():
     # Initialize the environment interface
     interface = GymEnvInterface(args.env_name, render_mode="human")
     obs_space = interface.env.observation_space
-    
+
     is_image_based = len(obs_space.shape) == 3
 
     if is_image_based:
@@ -56,17 +59,17 @@ def main():
     action_space = interface.env.action_space
     if isinstance(action_space, gym.spaces.Discrete):
         action_dim = action_space.n
-    else: # Box, etc.
+    else:  # Box, etc.
         action_dim = action_space.shape[0]
 
-    memory_args = {"d_model": 128, "nhead": 8} 
+    memory_args = {"d_model": 128, "nhead": 8}
     controller_args = {"action_dim": action_dim}
-
+    print(vision_model)
     # Initialize the World Model
     world_model = WorldModel(
         vision_model=vision_model,
         memory_model=TemporalTransformer,
-        controller_model=ModelPredictiveController,
+        controller_model=StochasticController,  #ModelPredictiveController,
         input_shape=input_shape,
         vision_args=vision_args,
         memory_args=memory_args,
@@ -78,9 +81,10 @@ def main():
 
     # Save the trained model
     world_model.save(f"{args.save_path}{args.env_name}_world_model.pt")
-    
+
     # Close the environment
     interface.close()
+
 
 if __name__ == "__main__":
     main()
