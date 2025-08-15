25ba458f4377353d91cd28c56b4c93adef681557
Larwive <larwive@gmail.com>
2025-08-15 20:13:09 +0200

    Add stochastic controller, add REINFORCE loss, finish integration

--- src/interface/main.py
+++ src/interface/main.py
@@ -1,11 +1,11 @@
-from interface import GymEnvInterface 
-
+from interface import GymEnvInterface
 
 # Example usage with CartPole-v1
-interface = GymEnvInterface("CartPole-v1", render_mode="human")
+interface = GymEnvInterface("CarRacing-v3", render_mode="human")  # CartPole-v1
 state = interface.reset()
 
 while not interface.done:
+
     action = interface.env.action_space.sample()  # Replace with your model's action
     next_state, reward, done, info = interface.step(action)
     interface.render()
