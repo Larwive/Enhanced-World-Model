1c217ab8baeb79c29d7cffbd206ff6f63f3210a8
Larwive <larwive@gmail.com>
2025-09-27 22:03:44 +0200

    Add tensorboard for gradient monitoring

--- src/train.py
+++ src/train.py
@@ -3,11 +3,43 @@
 import torch
 import numpy as np
 import gymnasium as gym
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.tensorboard.summary import hparams
+
 from interface.interface import GymEnvInterface
 from WorldModel import WorldModel
 
 
-def step(model, state, optimizer, device, is_image_based, action_space, reward):
+class SummaryWriter(SummaryWriter):
+    """
+    Add possiblity to store hyperparameters.
+    """
+
+    def add_hparams(self, hparam_dict, metric_dict):
+        torch._C._log_api_usage_once("tensorboard.logging.add_hparams")
+        if type(hparam_dict) is not dict or type(metric_dict) is not dict:
+            raise TypeError('hparam_dict and metric_dict should be dictionary.')
+        exp, ssi, sei = hparams(hparam_dict, metric_dict)
+
+        logdir = self._get_file_writer().get_logdir()
+
+        with SummaryWriter(log_dir=logdir) as w_hp:
+            w_hp.file_writer.add_summary(exp)
+            w_hp.file_writer.add_summary(ssi)
+            w_hp.file_writer.add_summary(sei)
+            for k, v in metric_dict.items():
+                w_hp.add_scalar(k, v)
+
+
+def step(model,
+         state,
+         optimizer,
+         device,
+         is_image_based,
+         action_space,
+         reward,
+         iter_num: int = 0,
+         tensorboard_writer=None):
     optimizer.zero_grad()
 
     if is_image_based:
@@ -23,17 +55,28 @@ def step(model, state, optimizer, device, is_image_based, action_space, reward):
     output_dict = model(state_tensor, return_losses=True, action_space=action_space)
     print(output_dict)
     total_loss = torch.sum(output_dict["total_loss"]) - (reward * output_dict["log_probs"]).mean()
+
+    if tensorboard_writer is not None:
+        tensorboard_writer.add_scalar("train/loss", total_loss.item(), iter_num)
+        for name, param in model.named_parameters():
+            if param.grad is not None:
+                tensorboard_writer.add_scalar(f"gradients/{name}", param.grad.norm().item(), iter_num)
+            else:
+                print("ERROR IN GRADIENTS ! {} is None".format(name))
+
     total_loss.backward()
     optimizer.step()
 
     return output_dict["action"], total_loss.item()
 
 
-def train(model: WorldModel, interface: GymEnvInterface, max_iter=10000, device='cpu'):
+def train(model: WorldModel, interface: GymEnvInterface, max_iter=10000, device='cpu', use_tensorboard: bool = True):
     print(model.parameters)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     is_image_based = len(interface.env.observation_space.shape) == 3
     action_space = interface.env.action_space
+
+    writer = SummaryWriter() if use_tensorboard else None
 
     last_reward = 0
 
@@ -43,7 +86,15 @@ def train(model: WorldModel, interface: GymEnvInterface, max_iter=10000, device=
         total_episode_loss = 0
 
         while not done:
-            action_tensor, loss = step(model, state, optimizer, device, is_image_based, action_space, last_reward)
+            action_tensor, loss = step(model,
+                                       state,
+                                       optimizer,
+                                       device,
+                                       is_image_based,
+                                       action_space,
+                                       last_reward,
+                                       iter_num=iter_num,
+                                       tensorboard_writer=writer)
 
             # Handle different action spaces
             if isinstance(action_space, gym.spaces.Discrete):
