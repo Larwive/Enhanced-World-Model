fix and features : Memory and Controller no longer have null gradient training looks fine

src/memory/CPC.py
@@ -12,8 +12,9 @@ class CPC(Model):
         self.predictors = torch.nn.ModuleList(
             [torch.nn.Linear(context_dim, latent_dim) for _ in range(prediction_steps)])
 
-    def forward(self, z_seq: torch.Tensor):
+    def forward(self, input: torch.Tensor):
         # z_seq: (B, T, D)
+        z_seq = input
         context, _ = self.gru(z_seq)
         predictions = [W(context) for W in self.predictors]
         return predictions
@@ -35,4 +36,5 @@ def info_nce_loss(z_seq, predictions, k_steps=12):
 
         loss = torch.nn.functional.cross_entropy(logits, labels)
         total_loss += loss
+        print(f"Step {k}, Loss: {loss.item()} {total_loss.item()}")
     return total_loss / k_steps

src/vision/Identity.py
@@ -15,12 +15,12 @@ class Identity(Model):
         self.embed_dim = input_shape[0]
 
     def forward(self, input):
-        # Return the input as the "reconstruction" and a zero loss
-        return input, torch.tensor(0.0, device=input.device)
-
-    def encode(self, input):
-        # The "encoded" version is just the input itself
-        return input.unsqueeze(-1).unsqueeze(-1) # Add dummy H, W dims for flatten_vision_latents
+        # Return a dictionary consistent with VQ_VAE
+        return {
+            "recon": input,
+            "vq_loss": torch.tensor(0.0, device=input.device),
+            "z_q": input.unsqueeze(-1).unsqueeze(-1)  # Add dummy H, W dims for flatten_vision_latents
+        }
 
     def export_hyperparam(self):
         return {