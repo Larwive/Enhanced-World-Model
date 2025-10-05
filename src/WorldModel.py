Minimal changes to fix null gradients from pull request #15

src/WorldModel.py
@@ -11,7 +11,6 @@ def squash_to_action_space(raw_action, action_space):
     """
     low = torch.as_tensor(action_space.low, dtype=torch.float32, device=raw_action.device)
     high = torch.as_tensor(action_space.high, dtype=torch.float32, device=raw_action.device)
-    high[2] = 0
     scaled = torch.sigmoid(raw_action) * (high - low) + low
     return scaled
 
@@ -57,7 +56,7 @@ class WorldModel(Model):
         # For vector-based models (Identity), z_q is (B, D, 1, 1).
         z_t = z_q.mean(dim=(2, 3))  # This works for both cases
 
-        h_t = self.memory(z_t)
+        h_t = self.memory(z_t.detach().clone())
 
         action, log_probs = self.controller(z_t, h_t)
 

src/train.py
@@ -60,7 +60,8 @@ def step(model,
     if tensorboard_writer is not None:
         tensorboard_writer.add_scalar("train/loss", total_loss.item(), iter_num)
         for name, param in model.named_parameters():
-            assert param is not None, "Parameter {} is None".format(name)
+            if iter_num and torch.isclose(torch.zeros_like(param.grad.norm()), param.grad.norm()): # Will ideally be removed in the future.
+                print("{}'s gradient is low ! ({})".format(name, param.grad.norm().item()))
             tensorboard_writer.add_scalar(f"gradients/{name}", param.grad.norm().item(), iter_num)
 
     optimizer.step()