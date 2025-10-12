9faa307da87b89a1ff7a896119cbddbf226769f3
Placeholder github Source User <larwive_placeholder_drvbjc@noreply.gitlab.bugz.site>
2025-10-12 17:27:39 +0000

    Handle cumulated reward

--- src/reward_predictor/LinearPredictor.py
+++ src/reward_predictor/LinearPredictor.py
@@ -10,10 +10,10 @@ class LinearPredictorModel(Model):
 
     def __init__(self, z_dim, h_dim, action_dim, **_kwargs):
         super().__init__()
-        self.linear = torch.nn.Linear(z_dim + h_dim + 1, 1) # Suspicious dim calculation. Need proper proof.
+        self.linear = torch.nn.Linear(z_dim + h_dim + 2, 1) # Suspicious dim calculation. Need proper proof.
 
-    def forward(self, z_t, h_t, log_prob):
-        x = torch.cat([z_t, h_t, log_prob.unsqueeze(-1)], dim=-1)  # Potential incorrect cat
+    def forward(self, z_t, h_t, log_prob, last_reward):
+        x = torch.cat([z_t, h_t, log_prob.unsqueeze(-1), torch.tensor([[last_reward]])], dim=-1)  # Potential incorrect cat
         return self.linear(x)
 
     def export_hyperparam(self):
