83219eb7b3218fe5097e7e5ed7bf714fdb0faa3b
Larwive <larwive@gmail.com>
2025-12-09 23:22:23 +0100

    Fix memory model parent class and prepare registering

--- src/memory/__init__.py
+++ src/memory/__init__.py
@@ -1,2 +1,12 @@
-from .TemporalTransformer import TemporalTransformer
-from .CPC import CPC
+from Model import Model
+
+REGISTRY = {}
+
+class MemoryModel(Model):
+    """
+    The base class for memory (M) models.
+    """
+
+    def __init_subclass__(cls, **kwargs):
+            super().__init_subclass__(**kwargs)
+            REGISTRY[cls.__name__] = cls
