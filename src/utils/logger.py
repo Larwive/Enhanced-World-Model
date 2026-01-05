8014c5009e3515f98c5c8a6c9f140e02209083bb
Larwive <larwive@gmail.com>
2026-01-05 21:50:07 +0100

    feature(all): Move tag implementation to frozenset Remove unused Color class, move Style class to StrEnum Refs: #62

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,8 +1,9 @@
 from typing import TextIO
 from sys import stdout
+from enum import StrEnum
 
 
-class Style:
+class Style(StrEnum):
     BLACK = "\033[0;30m"
     RED = "\033[0;31m"
     GREEN = "\033[0;32m"
